9b45b0718bbd0177e6f4d4f40ab94433b3db5569
sgavril <stefan.gavriliuc@ucalgary.ca>
2023-02-21 08:45:57 -0700

    fixed typo in settings to load static resources

--- btre/settings.py
+++ btre/settings.py
@@ -119,9 +119,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIR = [
-    os.path.join(BASE_DIR, 'btre/static')
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'btre/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
