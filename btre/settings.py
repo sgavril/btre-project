049227cbb54e263973ceeceae0c1cc77f3849e87
= <=>
2023-02-23 10:29:03 -0700

    Added admin functionality to view, filter and search through realtors and listings

--- btre/settings.py
+++ btre/settings.py
@@ -130,3 +130,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'btre/static')]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Media Folder Settings
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
