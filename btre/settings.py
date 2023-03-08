087859d3769d72235b3bd984f28d5ef63effa260
= <=>
2023-03-08 13:28:40 -0700

    Implemented contact form, emailing and dashboard functionality

--- btre/settings.py
+++ btre/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'listings.apps.ListingsConfig',
     'realtors.apps.RealtorsConfig',
     'accounts.apps.AccountsConfig',
+    'contacts.apps.ContactsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -142,3 +143,10 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'stefang1231@gmail.com'
+EMAIL_HOST_PASSWORD = 'mnramratearilhfu' # Application specific password
+EMAIL_USE_TLS=True
