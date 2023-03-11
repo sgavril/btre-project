73cf69082b4dc0e09463058abde17aa88f1e2e5e
= <=>
2023-03-11 09:33:28 -0700

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -150,3 +150,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER = 'stefang1231@gmail.com'
 EMAIL_HOST_PASSWORD = 'mnramratearilhfu' # Application specific password
 EMAIL_USE_TLS=True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
